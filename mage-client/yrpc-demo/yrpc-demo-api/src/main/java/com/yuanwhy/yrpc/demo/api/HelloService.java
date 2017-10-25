33cc4adf2d52fb4bbfd19304fff0326fb9257842
Hongyuan Wang <henrywangwhy@gmail.com>
2017-10-25 23:02:12 +0800

    rename yrpc to mage

---
+++ mage-client/yrpc-demo/yrpc-demo-api/src/main/java/com/yuanwhy/yrpc/demo/api/HelloService.java
@@ -0,0 +1,10 @@
+package com.yuanwhy.mage.demo.api;
+
+/**
+ * Created by hongyuan.wang on 23/10/2017.
+ */
+public interface HelloService {
+
+    String greeting(String name);
+
+}
---
+++ mage-client/yrpc-demo/yrpc-demo-server/src/main/java/com/yuanwhy/yrpc/server/ServerDemo.java
@@ -0,0 +1,12 @@
+package com.yuanwhy.mage.server;
+
+/**
+ * Created by hongyuan.wang on 23/10/2017.
+ */
+public class ServerDemo {
+
+    public static void main(String[] args) {
+        // TODO: 23/10/2017 start a mage server to serve
+    }
+
+}
